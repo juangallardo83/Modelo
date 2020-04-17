52b2ac76607fd8d526a1e2f4646f460adb1b6650
Juan Carlos Gallardo <j.gallardovasquez@uandresbello.edu>
2020-04-17 12:30:11 -0400

    primer commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h2> Bienvenido <h2>
+                    
+        </div>
+    </div>
+</div>
+@endsection
