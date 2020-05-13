7e28a05c06c33739149a81f23d8fe763c61dc5db
Juan Carlos Gallardo <j.gallardovasquez@uandresbello.edu>
2020-05-13 19:22:10 -0400

    Controles para cuestionario

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // return view('home');
+
+        $questionnaires = auth()->user()->questionnaires;
+
+        return view('home', compact('questionnaires'));
     }
 }
