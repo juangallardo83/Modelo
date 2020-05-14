cb2a1c4c6948b50f66c548a49034e37830c1c1b4
Juan Carlos Gallardo <j.gallardovasquez@uandresbello.edu>
2020-05-13 21:09:07 -0400

    modificación tabla de rutas

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,13 @@ Route::group(['middleware' => ['role:roladmin']], function () {
     Route::get('/usuarios', 'UserController@index');
 });
 
+Route::get('/questionnaires/create', 'QuestionnaireController@create');
+Route::post('/questionnaires', 'QuestionnaireController@store');
+Route::get('/questionnaires/{questionnaire}', 'QuestionnaireController@show');
+
+Route::get('/questionnaires/{questionnaire}/questions/create', 'QuestionController@create');
+Route::post('/questionnaires/{questionnaire}/questions', 'QuestionController@store');
+Route::delete('/questionnaires/{questionnaire}/questions/{question}', 'QuestionController@destroy');
+
+Route::get('/surveys/{questionnaire}-{slug}', 'SurveyController@show');
+Route::post('/surveys/{questionnaire}-{slug}', 'SurveyController@store');
